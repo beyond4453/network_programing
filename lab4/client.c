d1645d93d634f336e18c4060b747b39f21fc9791
ZhipengG <beyond_acm@163.com>
2014-12-22 13:36:02 +0800

    add lab4 source code

---
+++ lab4/client.c
@@ -0,0 +1,68 @@
+/**************************************************************************************************/
+/* Copyright (C) SA14226214, USTC, 2014-2015                                                      */
+/*                                                                                                */
+/*  FILE NAME             :  client.c                                                             */
+/*  PRINCIPAL AUTHOR      :  GaoZhipeng                                                           */
+/*  SUBSYSTEM NAME        :  lab1                                                                 */
+/*  MODULE NAME           :  client                                                               */
+/*  LANGUAGE              :  C                                                                    */
+/*  TARGET ENVIRONMENT    :  ANY                                                                  */
+/*  DATE OF FIRST RELEASE :  2014/12/01                                                           */
+/*  DESCRIPTION           :  This is a client program                                             */
+/**************************************************************************************************/
+
+/*
+ *Revision log:
+ *
+ *Ceated by GaoZhipeng, 2014/12/01
+ *     
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+#define MAXLINE 80
+#define SERV_PORT 8000
+
+int main(int argc, char *argv[]) 
+{
+	struct sockaddr_in servaddr;
+	char buf[MAXLINE];
+	int sockfd, n;
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
+	servaddr.sin_port = htons(SERV_PORT);
+
+	connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+	printf("input you message (q to exit): ");
+
+	//从标准输入设备中读取字符串写入buf
+	while(fgets(buf, MAXLINE, stdin) != NULL) {
+		if(buf[0] == 'q' && strlen(buf) == 2)
+        {
+            break;
+        }
+		write(sockfd, buf, strlen(buf));
+
+		n = read(sockfd, buf, MAXLINE);
+		if(n == 0)
+        {
+            printf("the connect side has been closed. please run it again\n");
+            exit(0);
+        }
+		write(STDOUT_FILENO, "Response from server : ", sizeof("Response from server : "));
+		write(STDOUT_FILENO, buf, n);
+		printf("\ninput you message (q to exit): ");
+	}
+	close(sockfd);
+	return 0;
+}
---
+++ lab4/readme.txt
@@ -0,0 +1,28 @@
+﻿此程序实现了基于C/S架构的TCP服务器：
+
+1.在linux下打开一个终端，先编译服务器代码
+$ gcc server.c -o server（会有一个警告，可以忽略）
+
+
+
+2.运行服务器
+$ ./server
+此时会打印Accepting conections ...，服务器会等待客户链接
+
+
+
+3.在linux下再开一个终端，编译客户端代码
+$ gcc client.c -o client
+
+
+
+4.运行客户端的代码
+$ ./client hello
+如果成功则会收到服务器返回的"hi".
+
+
+
+感谢您的测试，如果遇到任何问题，请联系：
+QQ： 393852478
+email : beyond_acm@163.com
+
